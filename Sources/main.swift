d5b04ece0155b3201db336493854b054674be616
Abdullah Ali <37246509+attaryz@users.noreply.github.com>
2026-06-12 17:08:39 +0300

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,6 +3,12 @@ import Cocoa
 @main
 struct ClipboardManagerApp {
     static func main() {
+        let runningApps = NSRunningApplication.runningApplications(withBundleIdentifier: "com.user.clipboardmanager")
+        if runningApps.count > 1 {
+            print("App is already running.")
+            exit(0)
+        }
+        
         let app = NSApplication.shared
         let delegate = AppDelegate()
         app.delegate = delegate
